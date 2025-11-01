edb559f18ffe085038697f315c38ea91f340ed6f
rynerson <lrry223@uky.edu>
2025-11-01 16:42:20 -0400

    Complete project. Add book_read toggle functionality.

--- script.js
+++ script.js
@@ -13,7 +13,29 @@ function Book(title, author, pages, hasRead){
         return info;
     }
     this.bookID = self.crypto.randomUUID();
+    
+    
 }
+/*function to toggle the read state of the books*/
+Book.prototype.ReadToggle = function(cardElement){
+         const toggle = cardElement.querySelector('.read_toggle');
+         const readP = cardElement.querySelector('.has_read');
+         
+        if(this.hasRead == "yes"){
+            toggle.textContent = "Not Read";
+            this.hasRead = "no";
+
+         }
+         else{
+            toggle.textContent = "Read";
+            this.hasRead = "yes";
+         }
+         readP.textContent = "Read?: " + this.hasRead;
+            
+        
+
+    }
+
 //take params, create a book, and then store it in the library
 function addBooksToLibrary(userTitle,userAuthor,userPages,userHasRead){
    
@@ -23,7 +45,7 @@ function addBooksToLibrary(userTitle,userAuthor,userPages,userHasRead){
     displayBook(myBook);
 
 }
-
+/*function for displaying a new book card */
 function displayBook(book){
   const container = document.getElementById('container');
   const bookCard = document.createElement("div");
@@ -35,26 +57,43 @@ function displayBook(book){
 
   const author_p = document.createElement("p");
   author_p.textContent = "Author: " + book.author;
+  
 
   const pages_p = document.createElement("p");
   pages_p.textContent = "Pages: " + book.pages;
 
   const has_read_p = document.createElement("p");
   has_read_p.textContent = "Read?: " + book.hasRead;
+  has_read_p.classList.add("has_read");
 
   const ID_P = document.createElement("p");
   ID_P.textContent = "Unique ID: " + book.bookID;
+
   const removeBtn = document.createElement("button");
   removeBtn.textContent = "Remove Book";
   removeBtn.classList.add("remove_btn")
   removeBtn.addEventListener("click", () => removeBook(book.bookID));
-  bookCard.append(title_p, author_p, pages_p, has_read_p, ID_P);
 
+  const readButton = document.createElement("button");
+  if(book.hasRead == "yes"){
+      readButton.textContent = "Read";
+   
+  }
+  else{
+    readButton.textContent = "Not Read";
+    
+  }
+  readButton.classList.add("read_toggle");
+  readButton.addEventListener("click", ()=>book.ReadToggle(bookCard));
+
+  bookCard.append(title_p, author_p, pages_p, has_read_p, ID_P);
   bookCard.appendChild(removeBtn);
+  bookCard.appendChild(readButton);
   container.appendChild(bookCard);
 
 
 }
+
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
@@ -68,6 +107,7 @@ closeButton.addEventListener("click", () => {
   dialog.close();
 });
 const form = document.querySelector('form');
+//get the information to be put in a card
 form.addEventListener('submit', function(event) {
             event.preventDefault(); // Prevent default form submission
 
@@ -85,19 +125,21 @@ form.addEventListener('submit', function(event) {
         
             
         });
-        function removeBook(id){
-          //remove book from array
-          const index = myLibrary.findIndex(book => book.bookID === id);
-           if (index !== -1) {
-            myLibrary.splice(index, 1);
-          }
-          //remove book from DOM
-          const container = document.getElementById("container");
-          const cardToRemove = container.querySelector(`[data-id="${id}"]`);
-          if (cardToRemove) {
-            cardToRemove.remove();
-          }
 
 
+function removeBook(id){
+    //remove book from array
+    const index = myLibrary.findIndex(book => book.bookID === id);
+      if (index !== -1) {
+          myLibrary.splice(index, 1);
         }
+      //remove book from DOM
+      const container = document.getElementById("container");
+      const cardToRemove = container.querySelector(`[data-id="${id}"]`);
+      if (cardToRemove) {
+        cardToRemove.remove();
+      }
+
+
+      }
         
